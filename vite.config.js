650027b7205f956dc8de0d28e7b131d493ffd6a5
Rudy Hamame <77045003+rudyhamame@users.noreply.github.com>
2026-08-01 14:40:01 -0400

    dfnsdf

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: ['index.html', 'services/index.html'],
+    },
+  },
   server: {
     host: '0.0.0.0',
   },
